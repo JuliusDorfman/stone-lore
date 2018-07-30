e9dda10f7564687fea0a2530c090867dff7432e8
JuliusDorfman <juliusdorfman@gmail.com>
2018-07-30 11:40:24 -0700

    initialized Murloc test route

--- api/routes.js
+++ api/routes.js
@@ -1,15 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
 /*
-* @Route GET /api/route/test
-* @Desc GET test
-* @Access Public
-*/
+ * @Route GET /api/routes/test
+ * @Desc GET test
+ * @Access Public
+ */
 
 router.get('/test', (req, res) => {
-  res.json({routerGet: "we hit router.get in routes.js"})
+  res.json({
+    routerGet: "we hit router.get in routes.js"
+  })
   console.log("Test Route from routes.js")
+})
+
+
+/*
+ * @Route GET /api/routes/murloc
+ * @Desc GET murloc
+ * @Access Public
+ */
+
+router.get('/murloc', (req, res) => {
+  axios.get('https://omgvamp-hearthstone-v1.p.mashape.com/cards/races/Murloc', {
+      headers: {
+        "X-Mashape-Authorization": 'Das4135R2Smsh5AbDKdQdqYDjURDp1WKsN8jsnETmcfXMAhtjN'
+      }
+    })
+    .then(result => {
+      // console.log("Murloc Route from routes.js", result)
+      return res.json({
+        murlocGet: result.data
+      })
+    })
 })
 
 module.exports = router;
--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -7,8 +7,13 @@ export default class Homepage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      getRoute: ''
+      testRoute: '',
+      submitVal: '',
+      returnVal: ''
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
 
   componentDidMount() {
@@ -16,12 +21,35 @@ export default class Homepage extends Component {
     axios.get('/api/routes/test').then(res => {
       console.log("Test route called from react client")
       return "Test Route Called"
-    }).then(result =>{
-      this.setState({getRoute: result})
+    }).then(result => {
+      this.setState({ testRoute: result })
     })
   }
 
 
+  handleChange(e) {
+    this.setState({ submitVal: e.target.value })
+  }
+
+  handleSubmit(e) {
+    console.log(`e\xa0` + this.state.submitVal)
+  }
+
+  handleKeyPress(e) {
+    if (e.key === 'Enter') {
+      console.log(`e\xa0` + this.state.submitVal)
+      this.getCard()
+    }
+  }
+
+  getCard(e) {
+    let card = this.state.submitVal;
+    axios.get('/api/routes/murloc').then(res => {
+      this.setState({ returnVal: res.data }, ()=> {
+        console.log(this.state.returnVal)
+      })
+    })
+  }
 
   render() {
     return (
@@ -29,8 +57,11 @@ export default class Homepage extends Component {
         <Header />
         Homepage
         <div className="container">
-          {this.state.getRoute}
+          {this.state.testRoute}
         </div>
+        <input type="text" name="search" placeholder="card" onChange={this.handleChange} onKeyPress={this.handleKeyPress} />
+        <input type="submit" name="search" value="submit" onClick={this.handleSubmit} />
+        {/* {this.state.returnVal} */}
       </div>
     )
   }
