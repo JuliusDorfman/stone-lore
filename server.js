ff0f11812a7e2b6b5861550054dd5d39da1c9319
JuliusDorfman <juliusdorfman@gmail.com>
2018-08-02 15:19:57 -0700

    added a heckin' '/' to server.js app.get

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use('/api/routes', stoneRoutes)
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
