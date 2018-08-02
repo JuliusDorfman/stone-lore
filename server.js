6261c47d85b4a02e2a4a1bb3e0e9a530ec33002a
JuliusDorfman <juliusdorfman@gmail.com>
2018-08-02 13:56:05 -0700

    changed from path.resolve to path.join see if this does anything

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use('/api/routes', stoneRoutes)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
