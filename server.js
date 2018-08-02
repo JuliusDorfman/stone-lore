ed59af0568488f2f08f97832228be965108d5697
JuliusDorfman <juliusdorfman@gmail.com>
2018-08-02 09:56:26 -0700

    heroku test deploy

--- server.js
+++ server.js
@@ -4,20 +4,26 @@ const port = process.env.PORT || 8080;
 const path = require('path');
 const bodyParser = require('body-parser');
 
+// Body Parser for middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 
 const stoneRoutes = require('./api/routes');
 app.use('/api/routes', stoneRoutes)
 
-
+// Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('/build'));
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-	});
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
 }
 
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port,
+  () =>
+  console.log(`Listening on port ${port}`)
+);
