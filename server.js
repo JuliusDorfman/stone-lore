c2e1ee345267dfb8bf2796d623737d4f33167f69
JuliusDorfman <juliusdorfman@gmail.com>
2018-08-01 19:15:35 -0700

    heroku deploy test

--- server.js
+++ server.js
@@ -2,13 +2,22 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 const path = require('path');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 const stoneRoutes = require('./api/routes');
 app.use('/api/routes', stoneRoutes)
+
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+	});
+}
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
