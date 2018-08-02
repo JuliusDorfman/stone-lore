38f7e311c618d082bc5aec20b288dc113ca8f079
JuliusDorfman <juliusdorfman@gmail.com>
2018-08-02 11:07:06 -0700

    heroku test deploy

--- server.js
+++ server.js
@@ -16,13 +16,12 @@ app.use('/api/routes', stoneRoutes)
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
-  //set static folder
   app.use(express.static('client/build'));
-}
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 
 app.listen(port,
