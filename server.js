f7161011ddcb255679f2482333d838231fd0c388
JuliusDorfman <juliusdorfman@gmail.com>
2018-08-03 12:42:34 -0700

    heroku test

--- server.js
+++ server.js
@@ -14,11 +14,20 @@ const stoneRoutes = require('./api/routes');
 app.use('/api/routes', stoneRoutes)
 
 // Serve static assets if in production
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, 'client/build')));
+//   app.get('/*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//   });
+// }
+
+
+// Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
+	app.use(express.static('/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+	});
 }
 
 const port = process.env.PORT || 8080;
