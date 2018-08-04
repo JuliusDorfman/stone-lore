1f43c48b7d1333ea28a4d35b998cc0319ea5fc0b
JuliusDorfman <juliusdorfman@gmail.com>
2018-08-04 13:40:29 -0700

    heroku

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ const stoneRoutes = require('./api/routes');
 app.use('/api/routes', stoneRoutes)
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.resolve(__dirname, '/client/build')));
-	app.get('/', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
-	});
-}
+// if (process.env.NODE_ENV === 'production') {
+// 	app.use(express.static(path.resolve(__dirname, '/client/build')));
+// 	app.get('/*', (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+// 	});
+// }
 
 const port = process.env.PORT || 8080;
 app.listen(port,
